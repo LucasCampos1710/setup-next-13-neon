fce8a03aab55d60382851447c86289ccfb3592bc
lucascampos1710 <soluxcreative@gmail.com>
2023-07-19 17:05:59 -0300

    feat: add conection with db and inputs name and phone

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,37 @@
-export default function Home() {
+import { db } from "@/db"
+import { users } from "@/db/schema";
+import { revalidatePath } from "next/cache";
+
+export default async function Home() {
+  const allUsers = await db.select().from(users);
+
+  async function addUser(data: FormData) {
+    "use server"
+
+    const fullName = data.get('full_name')?.toString()
+    const phone = data.get('phone')?.toString()
+
+    if (!fullName || !phone) {
+      return
+    }
+
+    await db.insert(users).values({
+      fullName,
+      phone,
+    })
+
+    revalidatePath('/')
+  }
+
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className="min-h-screen bg-zinc-900 text-zinc-50 flex items-center justify-center flex-col gap-6">
+      <p>{JSON.stringify(allUsers, null, 2)}</p>
+      <form action={addUser} className="flex flex-col gap-3">
+        <input type="text" name="full_name" placeholder="Full name" className="bg-zinc-800" />
+        <input type="text" name="phone" placeholder="Phone" className="bg-zinc-800" />
+
+        <button type="submit">Create</button>
+      </form>
     </div>
   )
 }
